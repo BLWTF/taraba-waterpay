6653470fd064f31813e459c174762dcd8e7750f1
BLWTF <badegbemile@gmail.com>
2021-05-08 08:59:52 +0100

    supercharging

--- resources/views/panels/horizontal-navbar.blade.php
+++ resources/views/panels/horizontal-navbar.blade.php
@@ -206,7 +206,7 @@
           <li class="dropdown dropdown-user nav-item">
             <a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
               <div class="user-nav d-lg-flex d-none">
-                <span class="user-name">John Doe</span>
+                <span class="user-name"></span>
               </div>
               <!-- <span><img class="round" src="{{asset('images/portrait/small/avatar-s-11.jpg')}}" alt="avatar" height="40" width="40"></span> -->
             </a>
